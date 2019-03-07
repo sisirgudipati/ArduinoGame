ee9dbd82099512691955957a43f43485bb9e8313
bsiever <bsiever@wustl.edu>
2019-03-07 12:44:58 -0600

    Initial commit.

---
+++ communication/SerialComm.java
@@ -0,0 +1,35 @@
+package communication;
+import jssc.*;
+
+public class SerialComm {
+
+	SerialPort port;
+
+	private boolean debug;  // Indicator of "debugging mode"
+	
+	// This function can be called to enable or disable "debugging mode"
+	void setDebug(boolean mode) {
+		debug = mode;
+	}	
+	
+
+	// Constructor for the SerialComm class
+	public SerialComm(String name) throws SerialPortException {
+		port = new SerialPort(name);		
+		port.openPort();
+		port.setParams(SerialPort.BAUDRATE_9600,
+			SerialPort.DATABITS_8,
+			SerialPort.STOPBITS_1,
+			SerialPort.PARITY_NONE);
+		
+		debug = false; // Default is to NOT be in debug mode
+	}
+		
+	// TODO: Add writeByte() method from Studio 5
+	
+	// TODO: Add available() method
+	
+	// TODO: Add readByte() method	
+	
+	// TODO: Add a main() method
+}
